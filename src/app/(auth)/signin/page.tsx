ebcb2f48fe7b3b0086242c26cce3686f5d16cd74
Raghav <anantraghav99@gmail.com>
2025-03-20 16:00:56 +0530

    login page redesing

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 import toast from "react-hot-toast";
 import { apiPost } from "@/helpers/axiosRequest";
 import { useRouter } from "next/navigation";
+import Image from "next/image";
 
 const signIn = () => {
   const router = useRouter();
@@ -14,7 +15,6 @@ const signIn = () => {
   });
   const [otp, setOtp] = useState("");
   const [showOtpInput, setShowOtpInput] = useState(false);
-
   const [isPasswordVisible, setIsPasswordVisible] = useState(false);
 
   const validateEmail = (email: string) => {
@@ -75,15 +75,20 @@ const signIn = () => {
 
   return (
     <div className={`flex items-center justify-center w-full h-screen ${Styles.mainContainer}`}>
+      <div className={Styles.logoTopLeft}>
+        <Image
+          src="https://swalay-music-files.s3.ap-south-1.amazonaws.com/assets/SwaLay++(1).png"
+          alt="logo"
+          width={250}
+          height={200}
+        />
+      </div>
       <div className={` ${Styles.containerLeft}`}>
         <div className={Styles.containerLeftInner}>
-          <h2 className={Styles.heading}>Sign In</h2>
+          <h2 className={`${Styles.heading}`}>Sign In</h2>
           <div>
-            <div className={`mt-3 ${Styles.registerform}`}>
-              <div className={Styles.formGroup}>
-                <label className={Styles.inputLable} htmlFor="email">
-                  <i className="bi bi-envelope-fill"></i>
-                </label>
+            <div className={`mt-5, ${Styles.registerform}`}>
+              <div className={Styles.formGroup}>               
                 <input
                   className={Styles.inputField}
                   type="email"
@@ -96,14 +101,6 @@ const signIn = () => {
                 />
               </div>
               <div className={Styles.formGroup}>
-                <label className={Styles.inputLable} htmlFor="pass">
-                  <i
-                    className={`bi ${
-                      isPasswordVisible ? "bi-eye-fill" : "bi-eye-slash-fill"
-                    } `}
-                    onClick={() => setIsPasswordVisible(!isPasswordVisible)}
-                  ></i>
-                </label>
                 <input
                   className={Styles.inputField}
                   type={isPasswordVisible ? "text" : "password"}
@@ -120,9 +117,6 @@ const signIn = () => {
 
               {showOtpInput && (
                 <div className={Styles.formGroup}>
-                  <label className={Styles.inputLable} htmlFor="otp">
-                    <i className="bi bi-shield-lock-fill"></i>
-                  </label>
                   <input
                     className={Styles.inputField}
                     type="text"
@@ -143,14 +137,47 @@ const signIn = () => {
                   {showOtpInput ? "Verify OTP" : "Sign In"}
                 </button>
               </div>
-              
             </div>
           </div>
         </div>
       </div>
       <div className={`flex h-screen ${Styles.containerRight}`}>
-        <h1 className={Styles.superHeading}>SwaLay</h1>
-        <p className={Styles.subHeading}>Get Your Music on Global Platforms</p>
+        <h1 className={`ms-10`}>
+          <Image
+            src={"https://swalay-music-files.s3.ap-south-1.amazonaws.com/assets/SwaLay+-2.png"}
+            alt="logo"
+            width={750}
+            height={0}
+          />
+          <p className={Styles.logoText}>Get Your Music on Global Platforms</p>
+        </h1>
+      </div>
+
+      {/* Music Icons at Random Positions */}
+      <div className={Styles.musicIconsContainer}>
+        <i className="bi bi-megaphone" style={{ position: 'absolute',  fontSize: '34px', color: '#e7e6e6' }}></i>
+      </div>
+      <div className={Styles.musicnoteIconsContainer}>
+        <i className="bi bi-music-note" style={{ position: 'absolute',  fontSize: '34px', color: '#e7e6e6' }}></i>
+      </div>
+      <div className={Styles.musicnote2IconsContainer}>
+        <i className="bi bi-music-note" style={{ position: 'absolute',  fontSize: '34px', color: '#e7e6e6' }}></i>
+      </div>
+      <div className={Styles.musicboomboxIconsContainer}>
+        <i className="bi bi-boombox" style={{ position: 'absolute',  fontSize: '34px', color: '#e7e6e6' }}></i>
+      </div> <div className={Styles.musicboombox2IconsContainer}>
+        <i className="bi bi-boombox" style={{ position: 'absolute',  fontSize: '34px', color: '#e7e6e6' }}></i>
+      </div>
+
+      <div className={Styles.musicnotebeamedIconsContainer}>
+        <i className="bi bi-music-note-beamed" style={{ position: 'absolute',  fontSize: '34px', color: '#e7e6e6' }}></i>
+      </div>
+      <div className={Styles.musicnotebeamed2IconsContainer}>
+        <i className="bi bi-music-note-beamed" style={{ position: 'absolute',  fontSize: '34px', color: '#e7e6e6' }}></i>
+      </div>
+
+      <div className={Styles.bottomrightContainer}>
+        <p>© 2025 SwaLay. All Rights Reserved.</p>
       </div>
     </div>
   );
